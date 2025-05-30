0e262daae9257d2b29a647010f1ca3f5719cb700
onurdoker <odoker@msn.com>
2025-05-30 23:54:11 +0300

    lesson12 - uploaded

---
+++ js/lesson12.js
@@ -0,0 +1,49 @@
+// OOP = Object Oriented Programming
+// a programming paradigm
+
+//procedural
+
+// const baseSalary = 30000;
+// const overtime = 10;
+// const rate = 20;
+//
+// const getWage = (baseSalary, overtime, rate) => {
+// 	return baseSalary + overtime * rate;
+// }
+// const finalWage = getWage(baseSalary, overtime, rate);
+// console.log(finalWage);
+
+// OOP
+// const employee = {
+// 	baseSalary: 30000,
+// 	overtime: 10,
+// 	rate: 20,
+// 	getWage() {
+// 		return this.baseSalary + this.overtime * this.rate;
+// 	}
+// }
+// const finalWage = employee.getWage();
+// console.log(finalWage);
+
+// class (soyut tanimlar footprint, tarif)
+// instance (olusan somut ornek)
+// interface (soyut arayuz)
+//
+
+// class User {
+// 	constructor(name, email) {
+// console.log('instance created');
+// console.log(name)
+// console.log(email)
+// 		(this.username = name), (this.useremail = email)
+// 	}
+// }
+
+// const mango = new User('Mango', 'mango@asd.com');
+// const poly = new User('Poly', 'poly@asd.com');
+// const mango = new User({
+// 	name: 'Mango',
+// 	email: 'mango@asd.com'
+// })
+// console.log(mango);
+// console.log(poly);

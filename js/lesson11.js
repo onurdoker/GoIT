a46771b6f7a2ddfa7371405f58d588ff7f1ad9a2
onurdoker <odoker@msn.com>
2025-05-30 23:54:05 +0300

    lesson11 - uploaded

---
+++ js/lesson11.js
@@ -0,0 +1,30 @@
+// const user = {
+// 	username: 'Victor',
+// 	age: 25,
+// 	showName() {
+// 		console.log(this.username);
+// 	}
+// }
+// // arrow function disinda fonksiyon cagirildiginda this'i kullanabiliriz.'
+// user.showName();
+
+// 'use strict'
+// function foo() {
+// 	console.log(this);
+// }
+// foo()
+
+// function greet(str) {
+// 	console.log(`${str}, ${this.username}`)
+// }
+// const mango = {
+// 	username: 'Mango',
+// 	room: 27
+// }
+// const poly = {
+// 	username: 'Poly',
+// 	room: 191
+// }
+// greet.call(mango, 'Welcome')
+// greet.call(poly, 'Hello')
+

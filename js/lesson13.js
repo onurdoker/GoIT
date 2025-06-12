99ef674f5246d00cb0717d14dcf9eb9821929ba9
onurdoker <odoker@msn.com>
2025-06-12 11:31:09 +0300

    lesson13.js- uploaded

---
+++ js/lesson13.js
@@ -0,0 +1,70 @@
+// DOM - Document Object Model
+// DOM manipulation
+
+//EcmaScript => JavaScript'in kurallari, kurlu olarak dusunelebilir
+
+// BOM - Browser Object Model
+// const listWithId = document.querySelector('menu');
+
+// listWithId.style.textTransform = 'uppercase';
+
+// listWithId.style.fontsize = '24px'
+
+// const link = document.querySelector('.link');
+// console.log(link);
+
+// const hasActiveClass = link.classlist.contains('is-  ');
+// const hasActiveClass2 = link.classList.contains('active');
+
+// link.classList.add('special');
+// console.log(list.classList);
+
+// link.classList.remove('is-active');
+// console.log(link.classList);
+
+// link.classList.toogle('is-active');
+// console.log(link.classList);
+
+// link.classList.replace('special', 'regular');
+// console.log(link.classList);
+
+// const heading = document.createElement('h1');
+
+// heading.classList.add('title');
+// heading.textContent = 'This is my new heading';
+// console.log(heading);
+
+// const image = document.createElement('img');
+// image.src = 'https://picsum.photoos/id/11/320/240';
+// image.alt = 'Nature';
+// console.log(image);
+
+// const list = document.querySelector('.username');
+
+// const lastItem = document.createElement('li');
+// lastItem.textContent = 'Poly';
+// list.append(lastItem);
+
+// const firstItem = document.createElement('li');
+// firstItem.textContent = 'Ajax';
+// list.prepend(firstItem);
+
+// append
+// prepend
+// before
+// after
+
+// const myNewHTML = '<li>Ajax2</li><li>Mango2</li>'
+// console.log(myNewHTML)
+
+// list.innerHTML = myNewHTML;
+
+const technologies = ['HTML', 'CSS', 'JavaScript', 'React'];
+const list = document.querySelector('.list');
+
+cont markup = technologies
+.map((technology)  => `<li class='list-item'>${technology}</li>` )
+.join('');
+
+// Adding all the markup in on operation
+list.innerHTML = markup;
